7c686ead5c82581d6cc7ce908f59bbca85025d6d
Alexander Rolek <a.rolek@gmail.com>
2015-03-15 16:17:07 -0700

    cleaned up errors on HttpDownload

--- download.go
+++ download.go
@@ -10,7 +10,7 @@ import (
 //	download a file via HTTP GET to a local destination
 //	url: absolute URL where the file is located
 //	dest: absolute path including the filename
-func HTTPdownload(url string, dest string) error {
+func HTTPdownload(url, dest string) (err error) {
 	//	make sure destination does not already exist
 	if FileExist(dest) {
 		return errors.New("dest file already exists")
@@ -19,33 +19,33 @@ func HTTPdownload(url string, dest string) error {
 	var err error
 	resp, err := http.Get(url)
 	if err != nil {
-		return err
+		return
 	}
 
 	// close the body on function complete
 	defer func() {
 		if err := resp.Body.Close(); err != nil {
-			panic(err)
+			return
 		}
 	}()
 
 	// open output file
 	file, err := os.Create(dest)
 	if err != nil {
-		return err
+		return
 	}
 
 	// close file on exit and check for error
 	defer func() {
 		if err := file.Close(); err != nil {
-			panic(err)
+			return
 		}
 	}()
 
 	_, err = io.Copy(file, resp.Body)
 	if err != nil {
-		return err
+		return
 	}
 
-	return nil
+	return
 }
