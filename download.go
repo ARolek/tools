4e91594647538b0ca17913cd0b9a64a4953b7890
Alexander Rolek <a.rolek@gmail.com>
2015-03-15 17:44:36 -0700

    defer err shadow fix

--- download.go
+++ download.go
@@ -22,24 +22,33 @@ func HTTPdownload(url, dest string) (err error) {
 	}
 
 	// close the body on function complete
-	defer func() {
-		if err = resp.Body.Close(); err != nil {
-			return
-		}
-	}()
+	defer resp.Body.Close()
 
+	/*
+		// close the body on function complete
+		defer func() {
+			if err = resp.Body.Close(); err != nil {
+				return
+			}
+		}()
+	*/
 	// open output file
 	file, err := os.Create(dest)
 	if err != nil {
 		return
 	}
 
+	// close file on function complete
+	defer file.Close()
+
 	// close file on exit and check for error
-	defer func() {
-		if err = file.Close(); err != nil {
-			return
-		}
-	}()
+	/*
+		defer func() {
+			if err = file.Close(); err != nil {
+				return
+			}
+		}()
+	*/
 
 	_, err = io.Copy(file, resp.Body)
 	if err != nil {
